022dcb9d534c908bab4a51b1598e0527c08b84e0
hestywahyuni <90597783+hestywahyuni@users.noreply.github.com>
2022-04-06 10:28:25 +0700

    wallpaper

---
+++ lib/page/categories.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_wallpaper/models/photo_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_wallpaper/widgets/widgets.dart';
+import 'dart:convert';
+import 'package:flutter_wallpaper/data/data.dart';
+
+class Categorie extends StatefulWidget {
+  final String categorieName;
+  Categorie({required this.categorieName});
+  @override
+  State<Categorie> createState() => _CategorieState();
+}
+
+class _CategorieState extends State<Categorie> {
+  List<photoModel> photo = [];
+
+  getSearchWallpapers(String query) async {
+    var response = await http.get(
+        Uri.parse(
+            "https://api.pexels.com/v1/search?query=$query&per_page=15&page=1"),
+        headers: {"Authorization": apiKEY});
+
+    // print(response.body.toString());
+
+    Map<String, dynamic> jsonData = jsonDecode(response.body);
+    jsonData["photos"].forEach((element) {
+      // print(element);
+      photoModel wallpaperModel = new photoModel();
+      wallpaperModel = photoModel.fromMap(element);
+      photo.add(wallpaperModel);
+    });
+
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getSearchWallpapers(widget.categorieName);
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: brandName(),
+          elevation: 0.0,
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+              child: Column(
+            children: [
+              SizedBox(
+                height: 16,
+              ),
+            ],
+          )),
+        ));
+  }
+}
---
+++ lib/page/search.dart
@@ -0,0 +1,88 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_walpaper/data/data.dart';
+import 'package:flutter_walpaper/models/photo_model.dart';
+import 'package:flutter_walpaper/widgets/widgets.dart';
+
+class Search extends StatefulWidget {
+  final String search;
+  Search({required this.search});
+
+  @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  TextEditingController search = TextEditingController();
+  List<photoModel> photo = [];
+
+  getSearchWallpapers(String query) async {
+    var response = await http.get(
+        Uri.parse(
+            "https://api.pexels.com/v1/search?query=$query&per_page=15&page=1"),
+        headers: {"Authorization": apiKEY});
+
+    Map<String, dynamic> jsonData = jsonDecode(response.body);
+    jsonData["photos"].forEach((element) {
+      // print(element);
+      photoModel photosModel = new photoModel();
+      photosModel = photoModel.fromMap(element);
+      photo.add(photosModel);
+    });
+
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getSearchWallpapers(widget.search);
+    super.initState();
+    search.text = widget.search;
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: brandName(),
+        elevation: 0.0,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: Color(0xfff5f8fd),
+                    borderRadius: BorderRadius.circular(30)),
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
+                margin: EdgeInsets.symmetric(horizontal: 24.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: search,
+                        decoration: InputDecoration(
+                            hintText: "Search Wallpaper",
+                            border: InputBorder.none),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        getSearchWallpapers(search.text);
+                      },
+                      child: Container(child: Icon(Icons.search)),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
